a4b4ad13ea0bb30248395fd1b579e36f5d78dab9
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-07-13 21:22:01 +0430

    Fix TextToUtf8Converter making the passed code empty
    Using PHP 8.1.7, passing null as the second argument of mb_detect_
    encoding() detects it as strange "UUENCODE" encoding, and converting
    it to UTF-8 gives an empty string. Pass mb_detect_order() instead,
    although it should do the same (but it does not). It might be an
    internal PHP bug.

--- src/Analysis/SourceCodeReading/TextToUtf8Converter.php
+++ src/Analysis/SourceCodeReading/TextToUtf8Converter.php
@@ -12,7 +12,9 @@ final class TextToUtf8Converter implements TextEncodingConverterInterface
      */
     public function convert(string $code): string
     {
-        $encoding = mb_detect_encoding($code, null, true);
+        // Passing null does NOT work with 8.1(.7), although based on PHP docs, it must do the same
+        // thing as calling mb_detect_order(). It might be an internal bug, but not totally sure.
+        $encoding = mb_detect_encoding($code, mb_detect_order(), true);
 
         if ($encoding === false) {
             $encoding = 'ASCII';
