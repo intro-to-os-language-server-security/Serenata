988a13006a0a4d6ce1e2d4f603acad7cd1c79d58
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-07-14 06:49:19 +0430

    Remove unnecessary branches from TextToUtf8Converter::convert()

--- src/Analysis/SourceCodeReading/TextToUtf8Converter.php
+++ src/Analysis/SourceCodeReading/TextToUtf8Converter.php
@@ -16,22 +16,16 @@ final class TextToUtf8Converter implements TextEncodingConverterInterface
         // The workaround is, we suppose the encoding is UTF-8 by default. If it is already, no
         // action will be done, otherwise, it tries to detect and convert the encoding. Notice that
         // the latter case would be a rare situation, so no worries.
-        $utf8 = mb_detect_encoding($code, ['UTF-8'], true);
+        $utf8 = \mb_detect_encoding($code, ['UTF-8'], true);
 
         if ($utf8 !== false) {
             return $code;
         }
 
-        $encoding = mb_detect_encoding($code, mb_detect_order(), true);
+        $encoding = \mb_detect_encoding($code, mb_detect_order());
 
-        if ($encoding === false) {
-            $encoding = 'ASCII';
-        }
-
-        if (!in_array($encoding, ['UTF-8', 'ASCII'], true)) {
-            $code = mb_convert_encoding($code, 'UTF-8', $encoding);
-        }
-
-        return $code;
+        // Safe assumption: $encoding is neither equal to UTF-8 nor ASCII
+        // (as every ASCII encoding is also a UTF-8 one)
+        return \mb_convert_encoding($code, 'UTF-8', $encoding);
     }
 }
